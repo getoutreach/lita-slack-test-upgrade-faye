cdeab9ee53be3788f66cd81379d157fec0b018b4
Chatchai Luangmanee <93616024+chatchai-outreach@users.noreply.github.com>
2023-03-16 10:42:54 -0700

    skip-verify

--- lib/lita/adapters/slack/rtm_connection.rb
+++ lib/lita/adapters/slack/rtm_connection.rb
@@ -103,8 +103,8 @@ module Lita
         end
 
         def websocket_options
-          # options = { ping: 10 , tls: {verify_peer: false}}
-          options = { ping: 10 , tls: {root_cert_file: '/etc/ssl/certs/ca-certificates.crt'}}
+          options = { ping: 10 , tls: {verify_peer: false}}
+          # options = { ping: 10 , tls: {root_cert_file: '/etc/ssl/certs/ca-certificates.crt'}}
           options[:proxy] = { :origin => config.proxy } if config.proxy
           options
         end
