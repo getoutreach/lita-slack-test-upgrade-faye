5a003e0b0098e33607ae7db921cc2f90f8be427d
Chatchai Luangmanee <93616024+chatchai-outreach@users.noreply.github.com>
2023-03-17 10:35:36 -0700

    test /usr/local/share/ca-certificates/slack-ca.pem

--- lib/lita/adapters/slack/rtm_connection.rb
+++ lib/lita/adapters/slack/rtm_connection.rb
@@ -107,7 +107,7 @@ module Lita
           # options = { ping: 10 , tls: {verify_peer: false}}
           # options = { ping: 10 , tls: {root_cert_file: '/invalid/cert/path'}}
           # options = { ping: 10 , tls: {root_cert_file: '/etc/ssl/certs/ca-certificates.crt'}}
-          options = { ping: 10 , tls: {root_cert_file: '/usr/local/share/ca-certificates/letsencrypt.pem'}}
+          options = { ping: 10 , tls: {root_cert_file: '/usr/local/share/ca-certificates/slack-ca.pem'}}
           options[:proxy] = { :origin => config.proxy } if config.proxy
           options
         end
