8edd96f380d58edaa15b58b22c94a63161ae61e9
Chatchai Luangmanee <93616024+chatchai-outreach@users.noreply.github.com>
2023-03-17 15:22:01 -0700

    test

--- lib/lita/adapters/slack/rtm_connection.rb
+++ lib/lita/adapters/slack/rtm_connection.rb
@@ -103,8 +103,9 @@ module Lita
         end
 
         def websocket_options
-          # options = { ping: 10 }
-          options = { ping: 10 , tls: {verify_peer: false}}
+          options = { ping: 10 }
+          options[:tls] = { :root_cert_file => ['/etc/ssl/certs/ca-certificates.crt'] }}
+          # options = { ping: 10 , tls: {verify_peer: false}}
           # options = { ping: 10 , tls: {root_cert_file: '/invalid/cert/path'}}
           # options = { ping: 10 , tls: {root_cert_file: '/etc/ssl/certs/ca-certificates.crt'}}
           # options = { ping: 10 , tls: {root_cert_file: '/usr/local/share/ca-certificates/slack-ca.pem'}}
